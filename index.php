9abb3f707ef77eef72fe5c9f7acb863cf7bdf41c
kumpulanremaja <47444671+kumpulanremaja@users.noreply.github.com>
2019-11-16 20:20:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 /**
- * Name:    Dor Three beta
- * Author:  Satria Wibawa
- * FB: https://www.facebook.com/HTTP200K
- * Github: https://github.com/satriawibawa
- * Created: 31/08/2018
+ * Name:    Kumpulan remaja
+ * Author:  kumpulanremaja
  *
  */
 function mintaOTP($nomor, $wait){
